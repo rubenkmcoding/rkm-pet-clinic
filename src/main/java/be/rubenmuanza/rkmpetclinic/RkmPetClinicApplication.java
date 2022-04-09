6d5601c202f728b3634911a226e5185467ae5142
Ruben Muanza <galliano1986@live.nl>
2022-04-10 00:12:59 +0200

    Initial commit

---
+++ src/main/java/be/rubenmuanza/rkmpetclinic/RkmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package be.rubenmuanza.rkmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RkmPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/rubenmuanza/rkmpetclinic/RkmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package be.rubenmuanza.rkmpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkmPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
