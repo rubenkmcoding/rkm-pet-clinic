150d4e5f8d4bda779e2953baa141506ae34c93e5
Ruben Muanza <galliano1986@live.nl>
2022-04-10 23:57:40 +0200

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/model/Person.java
@@ -4,7 +4,7 @@ package be.rubenmuanza.rkmpetclinic.model;
 
 
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package be.rubenmuanza.rkmpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
