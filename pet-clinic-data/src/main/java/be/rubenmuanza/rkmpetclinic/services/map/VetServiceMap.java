51e46ce2466acfa178abe28cf99beb135dfb6e4c
Ruben Muanza <galliano1986@live.nl>
2022-04-18 21:27:22 +0200

    Refactored Map impl to abstract ID generation. Closes #24

--- pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/services/map/VetServiceMap.java
@@ -20,19 +20,17 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
     public void delete(Vet object) {
         super.delete(object);
-
     }
 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
-
     }
 
 
