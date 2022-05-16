9c98f893345058247751d4fbe4737a4006cd6631
Ruben Muanza <galliano1986@live.nl>
2022-05-16 23:24:35 +0200

    Added specialities, added to vet. closes #28

---
+++ pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package be.rubenmuanza.rkmpetclinic.services;
+
+import be.rubenmuanza.rkmpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/services/map/PetTypeServiceMapService.java
@@ -0,0 +1,44 @@
+package be.rubenmuanza.rkmpetclinic.services.map;
+
+import be.rubenmuanza.rkmpetclinic.model.PetType;
+import be.rubenmuanza.rkmpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMapService extends AbstractMapService<PetType, Long>implements PetTypeService {
+
+
+    @Override
+    public Set<PetType> findAll(){
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+
+
+
+
+}
