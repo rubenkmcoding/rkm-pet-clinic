9354b2adb3c52e1f290db264fe989acce2e25f7e
Ruben Muanza <galliano1986@live.nl>
2022-05-17 13:22:25 +0200

    Added Specialties Map impl. closes #32

---
+++ pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package be.rubenmuanza.rkmpetclinic.services;
+
+import be.rubenmuanza.rkmpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/be/rubenmuanza/rkmpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,43 @@
+package be.rubenmuanza.rkmpetclinic.services.map;
+
+import be.rubenmuanza.rkmpetclinic.model.Speciality;
+import be.rubenmuanza.rkmpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+
+
+
+
+
+}
